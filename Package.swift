ab4284db0eaee94beed272e92e47fea47c82d224
D'Alberti, Luca <luca.dalberti@merck.com>
2019-01-20 09:49:11 +0100

    Modified rocket pipeline

--- Package.swift
+++ Package.swift
@@ -13,15 +13,15 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/vapor/http.git", from: "3.1.6"),
 
-//        .package(url: "https://github.com/orta/PackageConfig.git", from: "0.0.1"), // dev
+        .package(url: "https://github.com/orta/PackageConfig.git", from: "0.0.1"), // dev
 
-//        .package(url: "https://github.com/orta/Komondor", from: "1.0.0"), // dev
+        .package(url: "https://github.com/orta/Komondor", from: "1.0.0"), // dev
 
-//        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.35.8"), // dev
+        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.35.8"), // dev
 
-//        .package(url: "https://github.com/Realm/SwiftLint", from: "0.28.1"), // dev
+        .package(url: "https://github.com/Realm/SwiftLint", from: "0.28.1"), // dev
 
-//        .package(url: "https://github.com/f-meloni/Rocket", from: "0.4.0"), // dev
+        .package(url: "https://github.com/f-meloni/Rocket", from: "0.4.0"), // dev
     ],
     targets: [
         .target(
@@ -53,10 +53,18 @@ let package = Package(
                 ["script": ["content": "echo \"Releasing $VERSION\""]],
                 "hide_dev_dependencies",
                 ["git_add": ["paths": ["Package.swift"]]],
-                ["commit": ["message": "Release of version $VERSION"]],
+                ["commit": [
+                    "message": "Release of version $VERSION",
+                    "no_verify": true,
+                ]],
                 "tag",
-                "push",
                 "unhide_dev_dependencies",
+                ["git_add": ["paths": ["Package.swift"]]],
+                ["commit": [
+                    "message": "Unhide dev dependencies",
+                    "no_verify": true,
+                ]],
+                "push",
                 ["script": ["content": "echo \"Version $VERSION release 🎉\""]],
         ]],
     ])
